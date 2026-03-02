33f5124e1ae6df19ea5d150f219098befe30674d
harshitajain2612 <harshitajain2612@gmail.com>
2026-03-02 23:37:01 +0530

    Update IncrementOp practice task

--- PracticeTasks/03_IncrementOp.js
+++ PracticeTasks/03_IncrementOp.js
@@ -1,7 +1,7 @@
 let a = 5; //5
 let b = a++ + ++a;//5+7 = 12                
 
-let c = a++ + ++a + ++a + ++a + a++;
+let c = a++ + ++a + ++a + ++a + a++; //7 + 8 + 9 + 11 + 11 = 48
 
 console.log(b);
 console.log(a);
