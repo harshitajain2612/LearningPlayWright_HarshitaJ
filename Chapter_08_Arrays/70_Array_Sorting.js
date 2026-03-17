09be963bc7becc3be2f35168ef88a6e5aadd5775
harshitajain2612 <harshitajain2612@gmail.com>
2026-03-17 12:00:36 +0530

    chore: commit workspace changes and push

--- Chapter_08_Arrays/70_Array_Sorting.js
+++ Chapter_08_Arrays/70_Array_Sorting.js
@@ -5,3 +5,20 @@ let fruits = ["banana","apple","cherry"];
 fruits.sort();
 
 console.log(fruits);
+
+//Alphabetical by default
+
+
+let num = [10,1,21,2];
+num.sort();//[ 1, 10, 2, 21 ] --> wrong.. compares as strings--sort converts the number into string if we want to sort it naturally then we will have to use below functions
+
+//console.log(num);
+
+num.sort((a,b) => a - b); //Acending
+
+console.log(num);
+
+num.sort((a,b) => b - a); //Descending
+
+console.log(num);
+
---
+++ Chapter_08_Arrays/72_Arrays_CheckingArrays.js
@@ -0,0 +1,33 @@
+//Checking arrays:
+
+//Check if something is an array or not
+
+let result = Array.isArray([1,2,3]);
+console.log(result);//true
+
+let result2 = Array.isArray([""]);
+console.log(result2);//true
+
+let result3 = Array.isArray("a");
+
+console.log(result3);//false
+
+//every function and some function
+
+let r = [80,90,85].every(s => s>=70);
+console.log(r);//true
+
+
+let r1 = [80,40,85].every(s => s<=70);
+console.log(r1);//false //if any of the element does not satisfy the condition, it returns false
+
+//Some - AAtleast one must pass
+
+let s1 = [80,40,85].some(s => s>=70);
+console.log(s1);//true
+
+
+let s2 = [80,90,85].some(s => s<70);
+console.log(s2);//true
+
+
